3dcdf44babc9e9829bc5dacf1a99127c4a80d4c8
David Whitney <david@davidwhitney.co.uk>
2013-02-21 12:57:54 +0000

    Hacked out code to set protocol handler.

--- RemoteUTorrentClickHandler/Features/AddMagnetHandlerCommand.cs
+++ RemoteUTorrentClickHandler/Features/AddMagnetHandlerCommand.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 
 namespace RemoteUTorrentClickHandler.Features
 {
     class AddMagnetHandlerCommand
     {
+        public void Exeute()
+        {
+            var regKey = Registry.CurrentUser.OpenSubKey("Software\\Classes", true);
+
+            var magnetRoot = regKey.CreateSubKey(@"Magnet");
+            magnetRoot.SetValue(string.Empty, "Magnet URI");
+            magnetRoot.SetValue("Content Type", "application/x-magnet");
+            magnetRoot.SetValue("URL Protocol", string.Empty);
+            magnetRoot.CreateSubKey(@"DefaultIcon").SetValue(string.Empty, "c:\\path\\to\\ico.ico");
+            magnetRoot.CreateSubKey(@"shell").SetValue(string.Empty, "open");
+            magnetRoot.CreateSubKey(@"shell\\open\\command").SetValue(string.Empty, "C:\\Program Files (x86)\\uTorrent\\uTorrent.exe\" \"%1\"");
+        }
     }
 }
--- RemoteUTorrentClickHandler/Program.cs
+++ RemoteUTorrentClickHandler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RemoteUTorrentClickHandler.Features;
 
 namespace RemoteUTorrentClickHandler
 {
@@ -10,6 +11,8 @@ namespace RemoteUTorrentClickHandler
     {
         static void Main(string[] args)
         {
+            var aa = new AddMagnetHandlerCommand();
+            aa.Exeute();
         }
     }
 }
